127d5faa7ebd5c7fe0f6f7fbb2714cda800c4529
Aziz Azizli <azizliaziz025@gmail.com>
2024-08-27 22:26:05 +0400

    add navigate function for comment

--- client/src/components/Comment/index.jsx
+++ client/src/components/Comment/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Comment = ({comment,name,date,userId}) => {
+const Comment = ({ comment, name, date, userId }) => {
+  const navigate = useNavigate()
   return (
     <div className="border-b border-black pb-2 mb-2">
-          <div className='flex justify-between items-center '><p className="break-all  text-gray-600 text-sm ">Name: <span className='font-semibold'>{name}</span></p> <p className="text-sm">{ date}</p></div>
+          <div className='flex justify-between items-center '><p className="break-all  text-gray-600 text-sm ">Name: <span onClick={()=>navigate(`/user/profile/userId=${userId}`)} className='font-semibold'>{name}</span></p> <p className="text-sm">{ date}</p></div>
           <p className="break-all text-gray-700">{ comment}</p>
   </div>
   )
